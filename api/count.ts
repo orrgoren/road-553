450cbaba28feaf952eaa9dc05e0f2a4a3a856f85
Orr Goren <orrg@pixellot.tv>
2026-03-28 20:19:51 +0300

    feat: add admin data

--- api/count.ts
+++ api/count.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-import { initDb } from './_db'
+import { initDb } from './_db.js'
 
 export default async function handler(_req: VercelRequest, res: VercelResponse) {
   const db = await initDb()
